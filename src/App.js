b03dda0467baeb6235cd45cc596b30ecda6499a1
thiagoasiahub <thiagoasiahub>
2023-01-07 18:56:24 -0300

    15th commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    const response = await fetch('/', {
+    const response = await fetch('http://15.229.15.104:3000', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
