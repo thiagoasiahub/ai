fba83d19d78513dc04e87446b6b9095e6ddaf2d2
thiagoasiahub <thiagoasiahub>
2023-01-08 21:59:06 -0300

    61 commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = null;
+const port = 3000;
 
 const configuration = new Configuration({
     organization: process.env.OPENAI_ORG_KEY,
